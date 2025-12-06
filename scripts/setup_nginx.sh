38e80c8d7ff88d6729134e138d03045bee59c2f1
Stanislav Mestechkin <s.mestechkin@gmail.com>
2025-12-06 16:02:54 +0000

    Add logging to bash script

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+logging(){
+    DATE=$(date "+%Y-%m-%d %T,%3N")
+    case $# in
+    0)
+        logging 'ERROR' 'No arguments passed to function logging in script setup_nginx.sh'
+        exit 1
+    ;;
+    1)
+        SEVERITY='INFO'
+        MESSAGE=$1
+    ;;
+    2)
+        SEVERITY=$1
+        MESSAGE=$2
+    ;;
+    *)
+        logging 'ERROR' 'Too many arguments passed to function logging in script setup_nginx.sh'
+        exit 1
+    ;;
+    esac
+
+    echo "$DATE - $SEVERITY - $MESSAGE">>logs/provisioning.log
+}
 
 print(){
     echo '-----------------------------------------------'
@@ -10,8 +33,11 @@ apt update -y
 # Install ngnix
 if [ $( apt list --installed -a  nginx | egrep -i '^nginx' | wc -l ) -eq 0 ]; then
     print 'Instalation of NGINX started...'
+    logging 'Instalation of NGINX started...'
     apt install nginx -y
     print  'Installation completed!'
+    logging 'Installation completed!'
 else
     print 'NGINX  already installed!'
+    logging 'WARNING' 'NGINX  already installed!'
 fi
